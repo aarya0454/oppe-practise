035c3c80cc1dca989196f23da74d76fbb8090d98
lakshya <developer.aary@gmail.com>
2025-08-11 00:35:03 +0530

    understand bfs and some oof it's aplications

--- dfs.py
+++ dfs.py
@@ -7,3 +7,42 @@ def dfs_adj_mat(matrix,start):
     visited = [False]*len(matrix)
     _dfs_adj_mat(matrix,start,visited)
     return visited
+
+def _dfs_adj_lst(a_list,node,visited,parent):
+    visited[node]=True
+    for v in a_list[node]:
+        if not visited[v]:
+            parent[v]=node
+            _dfs_adj_lst(a_list,v,visited,parent)
+def dfs_adj_lst(a_list,start):
+    visited = {i:False for i in a_list}
+    parent = {i:-1 for i in a_list}
+    parent[start] = start
+    _dfs_adj_lst(a_list,start,visited,parent)
+    return visited,parent
+
+
+
+def dfs_pre_post_utils(a_list,node,visited,pre,post,count,edges):
+    visited[node] = True
+    pre[node] = count
+    count+=1
+    for v in a_list[node]:
+        if not visited[node]:
+            edges.add((node,v))
+            dfs_pre_post_utils(a_list,v,visited,pre,post,count,edges)
+    post[node] = count
+    count +=1
+
+
+
+
+
+def dfs_pre_post(a_list,start):
+    visited = {i:False for i in a_list}
+    pre = {i:0 for i in a_list}
+    post = {i:0 for i in a_list}
+    count =1
+    edges = {}
+    dfs_pre_post_utils(a_list,start,visited,pre,post,count,edges)
+    return visited,pre,post,edges
